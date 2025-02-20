01aa34aa099f0d18a7f202c012ebc61e01d21456
Junki1992 <junki.warner@me.com>
2025-02-20 14:07:56 +0900

    「日別売上の合計」の計算方法を追記

--- sales_analysis/analysis.py
+++ sales_analysis/analysis.py
@@ -7,6 +7,7 @@ file_path = os.path.join(script_dir, "sales_data.csv")
 df = pd.read_csv(file_path)
 
 # 日別売上合計
-daily_sales = df.groupby("売上日")["数量"].sum()
+df["売上金額"] = df["数量"] * df["単価"]
+daily_sales = df.groupby("売上日")["売上金額"].sum()
 
 print(daily_sales)
